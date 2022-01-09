efdf19488faf2ac28ab18080cdf44e3d489d29ba
Admir Ljevo <admir.ljevo@edu.fit.ba>
2022-01-09 03:17:51 +0100

    Dodani parovi za klađenje

--- src/app/Components/izbornik/izbornik.component.ts
+++ src/app/Components/izbornik/izbornik.component.ts
@@ -6,10 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./izbornik.component.css']
 })
 export class IzbornikComponent implements OnInit {
-
+  public isKliknut1: boolean=false;
+  public isKliknut2: boolean=false;
+  public isKliknut3: boolean=false;
+  public isKliknut4: boolean=false;
+  public isKliknut5: boolean=false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  Kliknut1() {
+    this.isKliknut1=!this.isKliknut1;
+    this.isKliknut2=false;
+    this.isKliknut3=false;
+    this.isKliknut4=false;
+    this.isKliknut5=false;
+
+
+  }
+
+  Kliknut2() {
+    this.isKliknut1=false;
+    this.isKliknut2=!this.isKliknut2;
+    this.isKliknut3=false;
+    this.isKliknut4=false;
+    this.isKliknut5=false;
+  }
+
+  Kliknut3() {
+    this.isKliknut1=false;
+    this.isKliknut2=false;
+    this.isKliknut3=!this.isKliknut3;
+    this.isKliknut4=false;
+    this.isKliknut5=false;
+  }
+  Kliknut4() {
+    this.isKliknut1=false;
+    this.isKliknut2=false;
+    this.isKliknut3=false;
+    this.isKliknut4=true;
+    this.isKliknut5=false;
+  }
+  Kliknut5() {
+    this.isKliknut1=false;
+    this.isKliknut2=false;
+    this.isKliknut3=false;
+    this.isKliknut4=false;
+    this.isKliknut5=true;
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {animate, style, transition, trigger} from "@angular/animations";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +11,10 @@ import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { BannersComponent } from './Components/banners/banners.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { IzbornikComponent } from './Components/izbornik/izbornik.component';
+import { SportsComponent } from './Components/sports/sports.component';
+import {MatExpansionModule} from "@angular/material/expansion";
+import { MatSliderModule } from '@angular/material/slider';
+import {AccordionModule} from "ngx-bootstrap/accordion";
 
 @NgModule({
   declarations: [
@@ -17,12 +22,19 @@ import { IzbornikComponent } from './Components/izbornik/izbornik.component';
     NavBarComponent,
     BannersComponent,
     FooterComponent,
-    IzbornikComponent
+    IzbornikComponent,
+    SportsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatSliderModule,
+    AccordionModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
